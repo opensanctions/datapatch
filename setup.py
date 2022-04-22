63a2d39eb560e0f92532dca98daf9e3c573eed64
Friedrich Lindenberg <friedrich@pudo.org>
2022-04-22 13:31:13 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="datapatch",
-    version="0.2.0",
+    version="0.2.1",
     author="Friedrich Lindenberg",
     author_email="friedrich@pudo.org",
     url="https://github.com/pudo/datapatch",
