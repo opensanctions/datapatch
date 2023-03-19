48c10d58d4b357783301e2da08418dfaa89e8332
Friedrich Lindenberg <friedrich@pudo.org>
2023-03-19 15:55:15 +0100

    Bump version: 1.0.0 → 1.0.1

--- datapatch/__init__.py
+++ datapatch/__init__.py
@@ -6,7 +6,7 @@ from datapatch.util import read_yaml_file, PathLike
 from datapatch.exc import LookupException
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["Lookup", "Result", "LookupException", "get_lookups", "read_lookups"]
 
 
