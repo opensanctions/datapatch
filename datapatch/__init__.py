644b311f5f480cdb22c4f3cd9c7d3ef83d6a0400
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-04 16:45:59 +0200

    pass-through debug flag

--- datapatch/__init__.py
+++ datapatch/__init__.py
@@ -10,15 +10,18 @@ __version__ = "1.0.2"
 __all__ = ["Lookup", "Result", "LookupException", "get_lookups", "read_lookups"]
 
 
-def get_lookups(data: Dict[str, Dict[str, Any]]) -> Dict[str, Lookup]:
+def get_lookups(
+    data: Dict[str, Dict[str, Any]],
+    debug: bool = False,
+) -> Dict[str, Lookup]:
     """Turn a mapping of configs into a dict of Lookups."""
     lookups = {}
     for name, config in data.items():
-        lookups[name] = Lookup(name, config)
+        lookups[name] = Lookup(name, config, debug=debug)
     return lookups
 
 
-def read_lookups(path: PathLike) -> Dict[str, Lookup]:
+def read_lookups(path: PathLike, debug: bool = False) -> Dict[str, Lookup]:
     """Read a set of named lookups from a JSON/YAML file."""
     data = read_yaml_file(path)
-    return get_lookups(data)
+    return get_lookups(data, debug=debug)
